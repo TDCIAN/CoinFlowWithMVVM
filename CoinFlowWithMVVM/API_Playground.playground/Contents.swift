654bef6df7c746ea012d2a7227532e1251ee1956
Jeongmin <tdcian71@gmail.com>
2021-01-08 20:07:40 +0900

    뉴스 리스트 받아오기 테스트

--- CoinFlowWithMVVM/API_Playground.playground/Contents.swift
+++ CoinFlowWithMVVM/API_Playground.playground/Contents.swift
@@ -1,3 +1,50 @@
 import UIKit
 
-var str = "Hello, playground"
+
+struct NewsResponse: Codable {
+    let articleArray: [Article]
+}
+
+struct Article: Codable {
+    let title: String
+    let link: String
+    let date: String
+    let timestamp: TimeInterval
+    let description: String
+    let imageURL: String
+    
+    enum CodingKeys: String, CodingKey {
+        case title
+        case link
+        case date
+        case timestamp
+        case description
+        case imageURL = "imageUrl"
+    }
+}
+
+let newsURL = URL(string: "http://coinbelly.com/api/get_rss")!
+let urlSession = URLSession.shared
+let taskWithNewsURL = urlSession.dataTask(with: newsURL) { (data, response, error) in
+    let successRange = 200..<300
+    
+    guard error == nil,
+          let statusCode = (response as? HTTPURLResponse)?.statusCode,
+          successRange.contains(statusCode) else {
+        return
+    }
+    
+    // 문제가 없는쪽
+    guard let responseData = data else { return }
+    
+    let decoder = JSONDecoder()
+    do {
+        let response = try decoder.decode([NewsResponse].self, from: responseData)
+        print("--> success: \(response.first?.articleArray)")
+    } catch {
+        print("--> err: \(error.localizedDescription)")
+    }
+    
+}
+
+taskWithNewsURL.resume()
