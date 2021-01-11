ec8c07b8098504dccafbdbddf360d655c72eb6c1
Jeongmin <tdcian71@gmail.com>
2021-01-12 01:30:42 +0900

    requestNewsList 메소드 작성

--- CoinFlowWithMVVM/Network/NetworkManager.swift
+++ CoinFlowWithMVVM/Network/NetworkManager.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+
+class NetworkManager {
+    
+    static let session = URLSession.shared
+    
+    static func requestCoinList() {
+        
+    }
+
+    static func requestCoinChartData() {
+        
+    }
+    
+    static func requestNewsList() {
+        let newsURL = URL(string: "http://coinbelly.com/api/get_rss")!
+        let taskWithNewsURL = session.dataTask(with: newsURL) { (data, response, error) in
+            let successRange = 200..<300
+            
+            guard error == nil,
+                  let statusCode = (response as? HTTPURLResponse)?.statusCode,
+                  successRange.contains(statusCode) else {
+                return
+            }
+            
+            guard let responseData = data else { return }
+            
+            let decoder = JSONDecoder()
+            do {
+                let response = try decoder.decode([NewsResponse].self, from: responseData)
+            } catch {
+                print("--> NewsList Error: \(error.localizedDescription)")
+            }
+            
+        }
+    }
+    
+}
