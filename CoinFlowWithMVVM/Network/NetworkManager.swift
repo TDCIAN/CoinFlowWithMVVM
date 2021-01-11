e81b3eace99c62dd97a87a01770a944960ec0bef
Jeongmin <tdcian71@gmail.com>
2021-01-12 01:40:54 +0900

    requestCoinChartData() 메소드 작성

--- CoinFlowWithMVVM/Network/NetworkManager.swift
+++ CoinFlowWithMVVM/Network/NetworkManager.swift
@@ -36,7 +36,24 @@ class NetworkManager {
     }
 
     static func requestCoinChartData() {
-        
+        let coinChartDataURL = URL(string: "https://min-api.cryptocompare.com/data/histohour?fsym=BTC&tsym=USD&limit=24")!
+        let taskWithCoinChartDataURL = session.dataTask(with: coinChartDataURL) { (data, response, error) in
+            let successRange = 200..<300
+            guard error == nil,
+                  let statusCode = (response as? HTTPURLResponse)?.statusCode,
+                  successRange.contains(statusCode) else {
+                return
+            }
+            
+            guard let responseData = data else { return }
+            let decoder = JSONDecoder()
+            do {
+                let response = try decoder.decode(ChartDataResponse.self, from: responseData)
+            } catch {
+                print("--> CoinChart Err: \(error.localizedDescription)")
+            }
+        }
+        taskWithCoinChartDataURL.resume()
     }
     
     static func requestNewsList() {
