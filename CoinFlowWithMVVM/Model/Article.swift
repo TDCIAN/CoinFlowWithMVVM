a5a340ff65c1d4c70d18e0a79d35f4534de1c8ed
Jeongmin <tdcian71@gmail.com>
2021-01-12 01:14:53 +0900

    Article 파일 작성

--- CoinFlowWithMVVM/Model/Article.swift
+++ CoinFlowWithMVVM/Model/Article.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+struct NewsResponse: Codable {
+    let articleArray: [Article]
+}
+
+struct Article: Codable {
+    let title: String
+    let link: String
+    let date: String
+    let timestamp: TimeInterval
+    let description: String
+    let imageURL: String
+    
+    enum CodingKeys: String, CodingKey {
+        case title
+        case link
+        case date
+        case timestamp
+        case description
+        case imageURL = "imageUrl"
+    }
+}
