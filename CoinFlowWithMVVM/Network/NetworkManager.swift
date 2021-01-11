4f1182cb21abec45fe968295585bc5aa20fd0166
Jeongmin <tdcian71@gmail.com>
2021-01-12 01:37:10 +0900

    requestCoinList 메소드 작성

--- CoinFlowWithMVVM/Network/NetworkManager.swift
+++ CoinFlowWithMVVM/Network/NetworkManager.swift
@@ -12,7 +12,27 @@ class NetworkManager {
     static let session = URLSession.shared
     
     static func requestCoinList() {
-        
+        let coinListURL = URL(string: "https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,DASH,LTC,ETC,XRP,BCH,XMR,QTUM,ZEC,BTG&tsyms=USD")!
+        let taskWithCoinListURL = session.dataTask(with: coinListURL) { (data, response, error) in
+            let successRange = 200..<300
+            guard error == nil,
+                  let statusCode = (response as? HTTPURLResponse)?.statusCode,
+                  successRange.contains(statusCode) else {
+                return
+            }
+            
+            guard let responseData = data else { return }
+            
+            let decoder = JSONDecoder()
+            do {
+                let response = try decoder.decode(CoinListResponse.self, from: responseData)
+                print("--> coinList Success: \(response.raw.btg)")
+            } catch {
+                print("--> CoinList Err: \(error.localizedDescription)")
+            }
+            
+        }
+        taskWithCoinListURL.resume()        
     }
 
     static func requestCoinChartData() {
