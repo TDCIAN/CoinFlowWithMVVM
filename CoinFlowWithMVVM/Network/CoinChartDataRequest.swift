1dec4164e357c4a2fcc8b1e39221a51d20a75ecf
Jeongmin <tdcian71@gmail.com>
2021-01-13 01:16:29 +0900

    CoinChartDataRequest 작성

--- CoinFlowWithMVVM/Network/CoinChartDataRequest.swift
+++ CoinFlowWithMVVM/Network/CoinChartDataRequest.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+struct CoinChartDataRequest: Request {
+    var method: HTTPMethod = .get
+    var params: RequestParam
+    var path: String
+    
+    init(period: Period, param: RequestParam) {
+        self.path = EndPoint.coinChartData + period.urlPath
+        self.params = param
+    }
+}
