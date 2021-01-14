9b09ce9c917e82c8cfa11ee1ce0aa71a51d6839b
Jeongmin <tdcian71@gmail.com>
2021-01-14 17:29:47 +0900

    집 활용 예시

--- CoinFlowWithMVVM/ZipPlayground.playground/Contents.swift
+++ CoinFlowWithMVVM/ZipPlayground.playground/Contents.swift
@@ -1,3 +1,13 @@
 import UIKit
 
 var str = "Hello, playground"
+
+let xAxis = ["mon", "tue", "wed"]
+let yAxis = [4, 6, 8]
+
+let zipped = zip(xAxis, yAxis)
+let tuples = zipped.map({(key:$0, value:$1)})
+
+print(tuples)
+
+
