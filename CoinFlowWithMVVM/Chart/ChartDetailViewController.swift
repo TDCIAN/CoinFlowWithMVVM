f300050e0fbd71945c34675c888cec4f0cb8399b
Jeongmin <tdcian71@gmail.com>
2021-01-15 18:06:42 +0900

    스택뷰 안의 버튼을 클릭하면 버튼 아래 하이라이트바가 이동하도록 변경

--- CoinFlowWithMVVM/Chart/ChartDetailViewController.swift
+++ CoinFlowWithMVVM/Chart/ChartDetailViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 class ChartDetailViewController: UIViewController {
     @IBOutlet weak var coinTypeLabel: UILabel!
     @IBOutlet weak var currentPriceLabel: UILabel!
+    @IBOutlet weak var highlightBar: UIView!
+    @IBOutlet weak var highlightBarLeading: NSLayoutConstraint!
     
     var coinInfo: CoinInfo!
     
@@ -26,6 +28,22 @@ class ChartDetailViewController: UIViewController {
         print("--> 꽂힌 정보 확인 => 키: \(coinInfo.key), 밸류: \(coinInfo.value)")
     }
 
+    @IBAction func dailyButtonTapped(_ sender: UIButton) {
+        moveHighlightBar(to: sender)
+    }
+    
+    @IBAction func weeklyButtonTapped(_ sender: UIButton) {
+        moveHighlightBar(to: sender)
+    }
+    
+    @IBAction func monthlyButtonTapped(_ sender: UIButton) {
+        moveHighlightBar(to: sender)
+    }
+    
+    @IBAction func yearlyButtonTapped(_ sender: UIButton) {
+        moveHighlightBar(to: sender)
+    }
+    
 }
 
 extension ChartDetailViewController {
@@ -44,5 +62,9 @@ extension ChartDetailViewController {
     private func updateCoinInfo(_ coinInfo: CoinInfo) {
         coinTypeLabel.text = "\(coinInfo.key)"
         currentPriceLabel.text = String(format: "%.1f", coinInfo.value.usd.price)
+    }
+    
+    private func moveHighlightBar(to button: UIButton) {
+        highlightBarLeading.constant = button.frame.minX
     }
 }
